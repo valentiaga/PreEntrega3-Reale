96a97cede63d66799025bb8766c1fc54824a4186
valentina reale <valenreale123@gmail.com>
2023-09-19 23:57:12 -0300

    Desestructuracion y Operaores Avanzados

--- js/main.js
+++ js/main.js
@@ -94,16 +94,15 @@ function obtenerTotalLocalStorage() {
 function guardarProdLocalStorage(producto) {
     let productos;
     productos = obtenerProductosLocalStorage();
-    //El curso seleccionado se agrega al Array
+    //El producto seleccionado se agrega al Array
     productos.push(producto);
     localStorage.setItem('productos', JSON.stringify(productos));
 }
 
-//comprobar que hayan elementos en el LS
+//devuelve los elementos que haya en el LS o un array vacio en caso de que no haya
 function obtenerProductosLocalStorage() {
-    let productosLS;
-
-    if (localStorage.getItem('productos') === null ? productosLS = [] : productosLS = JSON.parse(localStorage.getItem('productos')));
+    const productosLS = JSON.parse(localStorage.getItem('productos')) || [];
+    // if (localStorage.getItem('productos') === null ? productosLS = [] : productosLS = JSON.parse(localStorage.getItem('productos')));
     return productosLS;
 }
 
@@ -117,13 +116,17 @@ function leerLS() {
     total.innerHTML = obtenerTotalLocalStorage();
 
     productosLS.forEach(function (producto) {
+        
+        //Desestructuracion
+        const {imagen, nombre, precio, id} = producto;
+
         //Construimos el template
         const row = document.createElement("tr");
         row.innerHTML = `
-        <td><img src="${producto.imagen}" width="100"></td>
-        <td>${producto.nombre}</td>
-        <td>${producto.precio}</td>
-        <td><a href="#" class="borrar-producto" data-id="${producto.id}">X</a></td>    
+        <td><img src="${imagen}" width="100"></td>
+        <td>${nombre}</td>
+        <td>${precio}</td>
+        <td><a href="#" class="borrar-producto" data-id="${id}">X</a></td>    
     `;
         carritoProductos.appendChild(row);
     })
@@ -179,10 +182,8 @@ function eliminarProdLS(producto) {
     productosLS = obtenerProductosLocalStorage();
     //iteramo para buscar coincidencias y eliminar
     productosLS.forEach(function (productoLS, index) {
-        if (productoLS.id === producto) {
-            precio = productoLS.precio;
-            productosLS.splice(index, 1);
-        }
+        //Operador avanzado &&
+        productoLS.id === producto && productosLS.splice(index, 1);
     });
 
     localStorage.setItem('productos', JSON.stringify(productosLS));
@@ -196,131 +197,3 @@ function vaciarLs() {
 
 
 
-
-/*
-// Funcion inicial --- el usuario debe elegir accion a realizar
-function eligeOpcion() {
-    let opcionAccion = -1;
-    opcionAccion = prompt("Qué acción desea realizar? \n 1 - Buscar producto \n 2 - Ver listado de productos \n 3 - Finalizar");
-    while (opcionAccion != '3') {
-        while (opcionAccion != "1" && opcionAccion != "2" && opcionAccion != "3") {
-            alert("Ingrese una opción válida entre 1, 2 y 3");
-            opcionAccion = prompt("Qué acción desea realizar? \n 1 - Buscar producto \n 2 - Ver listado de productos \n 3 - Finalizar");
-        }
-        if (opcionAccion == '1') {
-            busquedaDisponibles();
-        }
-        else if (opcionAccion == '2') {
-            compra();
-        }
-        opcionAccion = prompt("Qué acción desea realizar? \n 1 - Buscar producto \n 2 - Ver listado de productos \n 3 - Finalizar");
-    }
-}
-
-// Funcion compra --- el usuario elige productos para agregar al carrito
-function compra() {
-    let indice = 0;
-    let i = 0;
-    let listado = "";
-    productosDisponibles.forEach((element, i) => {
-        count = i + 1;
-        listado += count + ". " + element.nombre + " " + element.caracteristica + "-------------- $" + element.precio + "\n";
-    });
-    let opcion = prompt("Ingrese el producto que desea: \n 0 - Finalizar \n" + listado);
-    while (opcion != "0") {
-        while (opcion < 0) {
-            alert("El numero ingresado debe ser mayor a 0");
-            opcion = prompt("Ingrese el producto que desea: \n " + listado);
-        }
-        cargaProductos(opcion, indice);
-        indice++;
-        opcion = prompt("Ingrese el producto que desea: \n 0 - Finalizar \n" + listado);
-    }
-    alert("El total de su compra es: $" + calculaTotalCompra() + " \nCarrito: \n" + muestraProductos());
-    alert("Gracias por su visita! 😁")
-}
-
-
-// Funcion calcula total de los productos del carrito
-function calculaTotalCompra() {
-    let totalCompra = 0;
-    productosElegidos.forEach((element, i) => {
-        totalCompra += element.precio;
-    });
-    return totalCompra;
-}
-
-
-// Funcion muestra productos del carrito
-function muestraProductos() {
-    let listadoElegidos = "";
-    productosElegidos.forEach((element) => {
-        listadoElegidos += element.cantidad + " " + element.nombre + " " + element.caracteristica + "-------------- $" + element.precio + "\n";
-    });
-    return listadoElegidos;
-}
-
-
-// Funcion ingresa productos al carrito
-function cargaProductos(opcion, indice) {
-    let cantidad;
-    productosElegidos[indice] = { nombre: productosDisponibles[opcion - 1].nombre, caracteristica: productosDisponibles[opcion - 1].caracteristica, cantidad: 0, precio: 0 };
-    // productosElegidos[indice] = productosDisponibles[opcion - 1];
-    cantidad = parseInt(prompt("Ingrese la cantidad de unidades de " + `${productosElegidos[indice].nombre} ${productosElegidos[indice].caracteristica}` + " que desea."));
-    while (cantidad < 0) {
-        alert("Ingrese una cantidad mayor o igual 0");
-        cantidad = parseInt(prompt("Ingrese la cantidad de unidades de " + `${productosElegidos[indice].nombre} ${productosElegidos[indice].caracteristica}` + " que desea."));
-    }
-    productosElegidos[indice].cantidad += cantidad;
-    productosElegidos[indice].precio = productosElegidos[indice].cantidad * productosDisponibles[opcion - 1].precio;
-    productosDisponibles[opcion - 1].stock = productosDisponibles[opcion - 1].stock - cantidad;
-}
-
-
-// Funcion busca producto ingresado por el usuario en los disponibles y los muestra
-function busquedaDisponibles() {
-    let productoBuscado = prompt("Ingrese su búsqueda:");
-    while (productoBuscado.length == 0) {
-        alert("Ingrese un producto o parte del mismo.");
-        productoBuscado = prompt("Ingrese su búsqueda:");
-    }
-    let arrayBusqueda = buscaProducto(productosDisponibles, productoBuscado);
-    if (arrayBusqueda.length > 0) {
-        let listadoBuscados = "";
-        arrayBusqueda.forEach((element) => {
-            listadoBuscados += element.nombre + " " + element.caracteristica + "-------------- $" + element.precio + "\n";
-        });
-        alert("Los productos disponibles son los siguientes: \n" + listadoBuscados);
-    }
-}
-
-function buscaProducto(productosDisponibles, productoBuscado) {
-    return productosDisponibles.filter((el) => el.nombre.includes(productoBuscado));
-}
-
-// eligeOpcion();
-*/
-
-// const inputUser = document.querySelector('#user'),
-// inputPass = document.querySelector('#pass'),
-// check = document.querySelector('#check'),
-// formulario = document.querySelector('#form-login'),
-// message = document.querySelector('#message');
-
-// function guardar(valor){
-//     const user = {
-//         usuario: inputUser.value,
-//         pass: inputPass.value    
-//         //validar que los campos no estén vacíos
-//     }
-//     if (valor == 'localStorage'){
-//         localStorage.setItem('user', JSON.stringify(user));
-//     }
-//     if (valor == 'sessionStorage'){
-//         localStorage.setItem('user', JSON.stringify(user));
-//     }
-// }
-
-// formulario.addEventListener('submit', ()=>{
-
-// })
